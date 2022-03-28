3c3a505917dcbffc11bc12b27b56bf91c449d359
Madaraka02 <victormadaraka02@gmail.com>
2022-03-28 09:17:09 -0700

    called db items

--- travel/urls.py
+++ travel/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', ),
+    path('', home, name="home"),
+    path('attractions/<slug:slug>/', attDetails, name="attdetails"),
+    path('parks/<slug:slug>/', parkDetails, name="parkdetails"),
+    path('search/', search, name='search'),
 ]
--- travel/views.py
+++ travel/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import *
 
-# Create your views here.
+def home(request):
+    attractions = Attraction.objects.all().order_by('-id')
+    parks = Park.objects.all().order_by('-id')
+    context = {
+        'attractions':attractions,
+        'parks':parks,
+    }
+    return render(request, 'home.html', context)
+
+def attDetails(request, slug):
+    att = get_object_or_404(Attraction, slug=slug)    
+    context = {
+        'att':att,
+    }
+    return render(request, 'attdetails.html', context)
+
+def parkDetails(request, slug):
+    park = get_object_or_404(Park, slug=slug)    
+    context = {
+        'park':park,
+    }
+    return render(request, 'parkdetails.html', context)    
+
+
+
+# class catlistView(ListView):
+#     template_name = "shop/blank.html"
+#     context_object_name = 'catlist'
+
+
+#     def get_queryset(self):
+#         content = {
+#             'cat': self.kwargs['category'],
+#             'items': Product.objects.filter(category__name=self.kwargs['category'])
+#         }
+#         return content 
+
+
+def search(request):
+    q = request.GET['q']
+    if q:
+        context = {
+            'data' : Product.objects.filter(name__icontains=q).order_by('-id'),
+        }
+
+        return render(request, 'shop/search.html', context)
+            
