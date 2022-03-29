077a8fd908d89429c0c8ed9d5485c4105f9eb717
Madaraka02 <victormadaraka02@gmail.com>
2022-03-28 22:57:06 -0700

    added blog urls

--- travel/urls.py
+++ travel/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
     path('parks/<slug:slug>/', parkDetails, name="parkdetails"),
     path('search/', search, name='search'),
     path('travel/admin/', admin, name='admin'),
+
     path('like/<slug:slug>/', like_blog, name="like_blog"),
     path('blogs/<slug:slug>/', blog_details, name="blog_details"),
+    
     path('blogs/', blogs, name="blogs"),
+    path('blog/edit/<int:id>/', edit_blog, name="edit_blog"),
+    path('blog/delete/<int:id>/', delete_blog, name="delete_blog"),
+    path('addblog/', postblog, name="postblog"),
 ]
