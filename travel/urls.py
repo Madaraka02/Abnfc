e8bf50dc52bbb885c9f18c7847607df4acda9abb
Madaraka02 <victormadaraka02@gmail.com>
2022-03-31 23:23:30 -0700

    added admin pages

--- travel/urls.py
+++ travel/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('', home, name="home"),
     path('attractions/<slug:slug>/', attDetails, name="attdetails"),
-    path('parks/<slug:slug>/', parkDetails, name="parkdetails"),
-    path('parks/', parks, name="parks"),
+    path('places/<slug:slug>/', parkDetails, name="parkdetails"),
+    path('places/', parks, name="parks"),
     path('attractions/', attractions, name="atts"),
     path('search/', search, name='search'),
     path('searchp/', searchp, name='searchp'),
@@ -24,9 +24,11 @@ urlpatterns = [
     # admin
     path('travel/admin/', admin, name='admin'),
     path('travel/admin/attractions/', admin_atts, name="admin_atts"),
-    path('travel/admin/parks/', admin_parks, name="admin_parks"),
+    path('travel/admin/places/', admin_parks, name="admin_parks"),
     path('travel/admin/att/add/', adminatt, name="adminatt"),
     path('travel/admin/park/add/', adminpark, name="adminpark"),
     path('travel/admin/att/edit/<int:id>/', edit_att, name="edit_att"),
-    path('travel/admin/park/edit/<int:id>/', edit_park, name="edit_park"),
+    path('travel/admin/place/edit/<int:id>/', edit_park, name="edit_park"),
+    path('travel/admin/att/delete/<int:id>/', delete_att, name="delete_att"),
+    path('travel/admin/place/delete/<int:id>/', delete_park, name="delete_park"),
 ]
