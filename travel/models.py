8d529c53c602a851b349e4348cb5782559b40216
Madaraka02 <victormadaraka02@gmail.com>
2022-04-01 04:49:59 -0700

    added comments to describe each model and url

--- travel/models.py
+++ travel/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 
-
+# database table to store all places
 class Park(models.Model):
     name = models.CharField(max_length=300, unique=True, blank=False)
     location = models.CharField(max_length=300, blank=False)
@@ -13,7 +13,7 @@ class Park(models.Model):
     def __str__(self):
         return self.name
 
-
+# database table to store all attractions
 class Attraction(models.Model):
     name = models.CharField(max_length=300, blank=False)
     image = models.FileField(upload_to='attimages')
@@ -23,7 +23,8 @@ class Attraction(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+# database table to store all multiple images of a specific attraction    
 class AttractionImages(models.Model):
     attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE)
     image = models.FileField(upload_to='attimagesm', blank=True, null=True)
@@ -31,7 +32,7 @@ class AttractionImages(models.Model):
     def __str__(self):
         return self.attraction.name
 
-
+# database table to store all multiple images of a specific place 
 class ParkImages(models.Model):
     park = models.ForeignKey(Park, on_delete=models.CASCADE) 
     image = models.FileField(upload_to='parksm', blank=True, null=True)   
@@ -40,6 +41,7 @@ class ParkImages(models.Model):
         return self.park.name
 
 
+# database table to store all blogs
 class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=500)
@@ -57,6 +59,7 @@ class Blog(models.Model):
     def total_likes(self):
         return self.likes.count() 
 
+# database table to store user feedback
 class Feedback(models.Model):
     name =  models.CharField(max_length=300)
     message = models.TextField() 
--- travel/urls.py
+++ travel/urls.py
@@ -2,33 +2,55 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # the homepage url
     path('', home, name="home"),
+    # url to a single attraction details page
     path('attractions/<slug:slug>/', attDetails, name="attdetails"),
+    # url to a single place details page
     path('places/<slug:slug>/', parkDetails, name="parkdetails"),
+    # url to all places page
     path('places/', parks, name="parks"),
+    # url to all attractions page
     path('attractions/', attractions, name="atts"),
+    # url to an attraction search results page
     path('search/', search, name='search'),
+    # url to a place search results page
     path('searchp/', searchp, name='searchp'),
     
-
+    # url to liking a single blog
     path('like/<slug:slug>/', like_blog, name="like_blog"),
+    # url to a single blog page
     path('blogs/<slug:slug>/', blog_details, name="blog_details"),
     
+    # url to all blogs page
     path('blogs/', blogs, name="blogs"),
+    # url to a single blog edit page
     path('blog/edit/<int:id>/', edit_blog, name="edit_blog"),
+    # url to a delete a single blog 
     path('blog/delete/<int:id>/', delete_blog, name="delete_blog"),
+    # url to post a blog
     path('addblog/', postblog, name="postblog"),
 
+    # url to a users dashboard page
     path('user/', userposts, name="user"),
 
-    # admin
+    # admin urls
+    # url to admin's home page
     path('travel/admin/', admin, name='admin'),
+    # url to admin's attractions page
     path('travel/admin/attractions/', admin_atts, name="admin_atts"),
+    # url to admin's places page
     path('travel/admin/places/', admin_parks, name="admin_parks"),
+    # url to admin's attractions add page
     path('travel/admin/att/add/', adminatt, name="adminatt"),
+    # url to admin's parks add page
     path('travel/admin/park/add/', adminpark, name="adminpark"),
+    # url to admin's attractions edit page
     path('travel/admin/att/edit/<int:id>/', edit_att, name="edit_att"),
+    # url to admin's places edit page
     path('travel/admin/place/edit/<int:id>/', edit_park, name="edit_park"),
+    # url to admin's attractions delete page
     path('travel/admin/att/delete/<int:id>/', delete_att, name="delete_att"),
+    # url to admin's places delete page
     path('travel/admin/place/delete/<int:id>/', delete_park, name="delete_park"),
 ]
